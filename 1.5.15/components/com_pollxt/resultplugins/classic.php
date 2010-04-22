5bb4a9a6d1f3b817c492127f75f5311e1746b913
ally <ally@478a4fa8-67ee-0310-8774-9eb661efe622>
2010-04-22 03:28:58 +0000

    add pollxt com frond end

---
+++ 1.5.15/components/com_pollxt/resultplugins/classic.php
@@ -0,0 +1,18 @@
+<?php 
+/**
+* PollXT for Joomla!
+* @Copyright ((c) 2004 - 2009 JoomlaXT
+* @ All rights reserved
+* @ Released under GNU/GPL License : http://www.gnu.org/copyleft/gpl.html
+* @ http://www.joomlaxt.com
+* @version 2.00.05
+**/
+defined('_JEXEC') or die('Direct Access to this location is not allowed.');
+?>
+
+<?php 
+
+class classicResult extends pollxtResultController {
+}
+
+?>
