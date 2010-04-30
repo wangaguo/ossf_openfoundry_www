27d6b348622a69aba63d3bb1db145e463d267863
ally <ally@478a4fa8-67ee-0310-8774-9eb661efe622>
2010-04-30 04:03:46 +0000

    add letterman code and rokstories and ofssologin

--- trunk/1.5.15/modules/mod_ofssologin/mod_ofssologin.php
+++ trunk/1.5.15/modules/mod_ofssologin/mod_ofssologin.php
@@ -1,27 +1,39 @@
 <?php
 $class_sfx = $params->get( 'moduleclass_sfx', "");
 $user = JFactory::getUser();
+
+
+global $_CB_framework, $_CB_database, $ueConfig, $mainframe, $_SERVER;
+include_once( $mainframe->getCfg( 'absolute_path' ) . '/administrator/components/com_comprofiler/plugin.foundation.php');
+//Add language select module
+include_once( $mainframe->getCfg( 'absolute_path' ) . '/modules/mod_jflanguageselection/mod_jflanguageselection.php');
+
+
+
 if($user->get('aid') == 0)
 { //logout
   $loginPost = ("sso/user/login?return_url=/");
   echo '<form action="'.$loginPost.'" method="post" id="mod_loginform'.$class_sfx.'" ';
-  //echo 'style="margin:0px;">'."\n";
-  //echo '<input type="submit" name="Submit" class="button" value="'.JText::_('LOGIN').'" />'; //login
-  echo '<a href="'.$loginPost.'">'.JText::_('Login').'</a>';
+  echo '<span class="mod_ssologin"><a href="'.$loginPost.'">'.JText::_('Login').'</a></span>';
 
   $singupPost = ("sso/user/signup");
-  echo '&nbsp;|&nbsp;<a href="'.$singupPost.'" class="mod_register'.$class_sfx.'">';
-  echo JText::_('Register'); 
-  echo '</a>';
+  echo '|<span class="mod_ssologin"><a href="'.$singupPost.'" class="mod_register'.$class_sfx.'">';
+  echo JText::_('REGISTER').'</span>'; 
+  echo '</a></span>';
+  echo '</form>';
 }
 else
 { //logout
-  $logoutPost = ("sso/user/logout");
-  echo '<form action="'.$logoutPost.'" method="post" id="mod_login_logoutform'.$class_sfx.'" style="margin:0px;">'."\n";
-
-  $txtusername = '<label for="mod_login_username'.$class_sfx.'">'.$user->get('username').'</label>';
-  echo '<span id="mod_login_usernametext'.$class_sfx.'">'.$txtusername.'</span>'; //username
-  echo '&nbsp;|&nbsp;<input type="submit" name="Submit" class="button" value="'.JText::_('Logout').'" />'; //logout
-  echo '<input type="hidden" name="return_url" value="'.JRequest::getURI().'"/>';
+  $logoutPost   = "sso/user/logout";
+  $dashboard    = "sso/user/dashboard";
+  $ssoedit      = "sso/user/edit";
+  $txtusername = '<span class="mod_ssologin">'.JText::_('HI');
+  $txtusername.= '<a href="'.cbSef("index.php?option=com_comprofiler&amp;task=userProfile&amp;Itemid=54").'" class="mod_login'.$class_sfx.'">';
+  $txtusername.= $user->get('username').'</a></span>';
+  echo '<span class="mod_ssologin">'.$txtusername.'</span>|'; //username
+  echo '<span class="mod_ssologin"><a href="'.$dashboard.'" >'.JText::_('DASHBOARD').'</a></span>|';
+  echo '<span class="mod_ssologin"><a href="'.$ssoedit.'">'.JText::_('SETTINGS').'</a></span>|';
+  echo '<span class="mod_ssologin"><a href="'.$logoutPost.'">'.JText::_('LOGOUT').'</a></span>';
 }
-echo '</form>';
+//Add search code
+include_once( $mainframe->getCfg( 'absolute_path' ) . '/modules/mod_rokajaxsearch/mod_rokajaxsearch.php');
