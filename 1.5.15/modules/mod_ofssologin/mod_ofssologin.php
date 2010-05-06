7706b1d6d93e93293801e8f968023c42e94823e2
wangaguo <wangaguo@gmail.com>
2010-05-06 05:40:22 +0000

    1. Add Search. 2. Adjust layout.

--- 1.5.15/modules/mod_ofssologin/mod_ofssologin.php
+++ 1.5.15/modules/mod_ofssologin/mod_ofssologin.php
@@ -1,39 +1,57 @@
 <?php
 $class_sfx = $params->get( 'moduleclass_sfx', "");
 $user = JFactory::getUser();
+$pipe = '&nbsp;&nbsp|&nbsp;&nbsp;';
 
 
-global $_CB_framework, $_CB_database, $ueConfig, $mainframe, $_SERVER;
-include_once( $mainframe->getCfg( 'absolute_path' ) . '/administrator/components/com_comprofiler/plugin.foundation.php');
-//Add language select module
-include_once( $mainframe->getCfg( 'absolute_path' ) . '/modules/mod_jflanguageselection/mod_jflanguageselection.php');
-
-
-
+global $mainframe;
+//include_once( $mainframe->getCfg( 'absolute_path' ) . '/administrator/components/com_comprofiler/plugin.foundation.php');
+echo '<div class="ofssologin_'.$class_sfx.'">';
 if($user->get('aid') == 0)
-{ //logout
-  $loginPost = ("sso/user/login?return_url=/");
-  echo '<form action="'.$loginPost.'" method="post" id="mod_loginform'.$class_sfx.'" ';
-  echo '<span class="mod_ssologin"><a href="'.$loginPost.'">'.JText::_('Login').'</a></span>';
-
+{ //login
+  $loginPost = ("/sso/user/login?return_url=/of/user/dashboard");
   $singupPost = ("sso/user/signup");
-  echo '|<span class="mod_ssologin"><a href="'.$singupPost.'" class="mod_register'.$class_sfx.'">';
-  echo JText::_('REGISTER').'</span>'; 
-  echo '</a></span>';
-  echo '</form>';
+  
+  echo '<a href="'.$loginPost.'">'.JText::_('Login').'</a>';
+  echo $pipe;
+  echo '<a href="'.$singupPost.'">'.JText::_('REGISTER').'</a>';
+  echo $pipe;
+  include_once( $mainframe->getCfg( 'absolute_path' ) . '/modules/mod_jflanguageselection/mod_jflanguageselection.php');
 }
 else
 { //logout
-  $logoutPost   = "sso/user/logout";
-  $dashboard    = "sso/user/dashboard";
-  $ssoedit      = "sso/user/edit";
-  $txtusername = '<span class="mod_ssologin">'.JText::_('HI');
-  $txtusername.= '<a href="'.cbSef("index.php?option=com_comprofiler&amp;task=userProfile&amp;Itemid=54").'" class="mod_login'.$class_sfx.'">';
-  $txtusername.= $user->get('username').'</a></span>';
-  echo '<span class="mod_ssologin">'.$txtusername.'</span>|'; //username
-  echo '<span class="mod_ssologin"><a href="'.$dashboard.'" >'.JText::_('DASHBOARD').'</a></span>|';
-  echo '<span class="mod_ssologin"><a href="'.$ssoedit.'">'.JText::_('SETTINGS').'</a></span>|';
-  echo '<span class="mod_ssologin"><a href="'.$logoutPost.'">'.JText::_('LOGOUT').'</a></span>';
+  $logoutPost   = "/sso/user/logout";
+  $dashboard    = "/of/user/dashboard";
+  $ssoedit      = "/sso/user/edit";
+  $txtusername  = $user->get('username');
+
+  echo JText::_('HI');
+  echo '<a href="'.cbSef("/index.php?option=com_comprofiler&amp;task=userProfile&amp;Itemid=54").'">';
+  echo $txtusername.'</a>';
+  echo $pipe;
+  echo '<a href="'.$dashboard.'" >'.JText::_('DASHBOARD').'</a>';
+  echo $pipe;
+  echo '<a href="'.$ssoedit.'">'.JText::_('SETTINGS').'</a>';
+  echo $pipe;
+  echo '<a href="'.$logoutPost.'">'.JText::_('LOGOUT').'</a>';
+  echo $pipe;
+  include_once( $mainframe->getCfg( 'absolute_path' ) . '/modules/mod_jflanguageselection/mod_jflanguageselection.php');
 }
 //Add search code
-include_once( $mainframe->getCfg( 'absolute_path' ) . '/modules/mod_rokajaxsearch/mod_rokajaxsearch.php');
+$doc =& JFactory::getDocument();
+$doc->addScript(JURI::root(true) ."/modules/mod_ofssologin/js/ofssologin.js");
+$JText = &JFactory::getLanguage();
+echo <<<EOD
+<div class="ofssologin_search_{$class_sfx}">
+  <form id="of_search" action="/of/openfoundry/search" method="get" onsubmit="of_search(); return false;">
+    <input id="query" name="query" id="mod_search_searchword" maxlength="20" alt="search" class="inputbox" type="text" size="28" value="search..."  onblur="if(this.value=='') this.value='search...';" onfocus="if(this.value=='search...') this.value='';" />
+    <select id="of_search_type">
+      <option value="Projects"> {$JText->_('PROJECTS')} </option>
+      <option value="Content"> {$JText->_('CONTENT')} </option>
+      <option value="Community"> {$JText->_('COMMUNITY')} </option>
+    </select>
+    <input type="hidden" name="commit" value="search" />
+  </form>
+</div>
+EOD;
+echo '</div>';
