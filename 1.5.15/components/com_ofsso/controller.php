c5c547af22fd171e0780a73fd06cbbef204ff589
wangaguo <wangaguo@gmail.com>
2010-05-06 08:21:01 +0000

    Add IP filter for security.

--- 1.5.15/components/com_ofsso/controller.php
+++ 1.5.15/components/com_ofsso/controller.php
@@ -11,3 +11,15 @@
 defined( '_JEXEC' ) or die( 'Restricted access' );
  
 jimport('joomla.application.component.controller');
+
+
+$REMOTE_ADDR = '';
+if(isset($_SERVER[HTTP_X_FORWARDED_FOR])) $REMOTE_ADDR = $_SERVER[HTTP_X_FORWARDED_FOR];
+else $REMOTE_ADDR = $_SERVER[REMOTE_ADDR];
+
+$config =& JFactory::getConfig();
+$ofsso_ip_limit = $config->getValue( 'ofsso_ip_limit' );
+if (!preg_match("/".$REMOTE_ADDR."/", $ofsso_ip_limit)) 
+{
+  die( 'Restricted access' );
+}
