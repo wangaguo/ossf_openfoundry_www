5caaafbc5943ac6bec1fef01ee78088c258b86bc
wangaguo <wangaguo@478a4fa8-67ee-0310-8774-9eb661efe622>
2010-06-29 07:07:20 +0000

    originally is return true/false. add CURLOPT_RETURNTRANSFER option, then return content.

--- trunk/1.5.15/components/com_comprofiler/plugin/user/plug_cbofpartnertab/cb.partnertab.php
+++ trunk/1.5.15/components/com_comprofiler/plugin/user/plug_cbofpartnertab/cb.partnertab.php
@@ -11,16 +11,13 @@ class getPartnerTab extends cbTabHandler {
 	
 	function getDisplayTab($tab,$user,$ui) {
 		global $_CB_framework, $_CB_database, $mainframe;
-		$return ='';
 		$username =str_replace ("!","",$user->username);
-	//    	$ch = curl_init();
-	//	curl_setopt($ch, CURLOPT_URL, "http://ssodev.openfoundry.org/of/api/user?do=partners&name=$username");
-//	//    echo "http://ssodev.openfoundry.org/of/api/user?do=partners&name=$username";
-	//	    $output = curl_exec($ch);
-	//	curl_close($ch); 
-		$return ="<iframe src=\"http://ssodev.openfoundry.org/of/api/user?do=partners&name=$username\" width=\"100%\"  height=\"100%\" scrolling=\"auto\" align=\"top\" frameborder=\"0\" lass=\"wrapper\"> Not Open </iframe>";	
-
-		return $return;
+	    $ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, "http://ssodev.openfoundry.org/of/api/user?do=partners&name=$username");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 ); 
+	    $output = curl_exec($ch);
+		curl_close($ch); 
+        return $output;
 	}
 }	// end class getPartnerTab.
 ?>
