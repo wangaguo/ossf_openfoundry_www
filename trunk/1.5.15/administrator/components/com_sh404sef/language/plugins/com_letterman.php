5c395ddcff1bafbc1f23fb925d4d397d3a5dfeed
ally <ally@478a4fa8-67ee-0310-8774-9eb661efe622>
2010-07-01 10:06:54 +0000

    add new sh404sef stable version

---
+++ trunk/1.5.15/administrator/components/com_sh404sef/language/plugins/com_letterman.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * SEF extension for Joomla!
+ *
+ * @author      $Author: shumisha $
+ * @copyright   Yannick Gaultier - 2007-2010
+ * @package     sh404SEF-15
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @version     $Id: com_letterman.php 1192 2010-04-04 16:11:04Z silianacom-svn $
+ */
+
+if (!defined('_JEXEC')) die('Direct Access to this location is not allowed.'); 
+  
+  // français
+  $sh_LANG['fr']['_COM_SEF_LETTERMAN'] = 'Lettre d information';
+  $sh_LANG['fr']['_COM_SEF_LM_CONFIRM'] = 'Confirmer adresse';
+  $sh_LANG['fr']['_COM_SEF_LM_SUBSCRIBE'] = 'S inscrire';
+  $sh_LANG['fr']['_COM_SEF_LM_UNSUBSCRIBE'] = 'Annuler son inscription';
+  // english
+  $sh_LANG['en']['_COM_SEF_LETTERMAN'] = 'Newsletter';
+  $sh_LANG['en']['_COM_SEF_LM_CONFIRM'] = 'Confirm address';
+  $sh_LANG['en']['_COM_SEF_LM_SUBSCRIBE'] = 'Subscribe';
+  $sh_LANG['en']['_COM_SEF_LM_UNSUBSCRIBE'] = 'Cancel subscription';
+  
+  // spanish
+  $sh_LANG['es']['_COM_SEF_LETTERMAN'] = 'Carta de informacion';
+  $sh_LANG['es']['_COM_SEF_LM_CONFIRM'] = 'Confirmar correo electronico';
+  $sh_LANG['es']['_COM_SEF_LM_SUBSCRIBE'] = 'Subscribir';
+  $sh_LANG['es']['_COM_SEF_LM_UNSUBSCRIBE'] = 'Cancelar subscripción';
+  // german
+  $sh_LANG['de']['_COM_SEF_LETTERMAN'] = 'Newsletter';
+  $sh_LANG['de']['_COM_SEF_LM_CONFIRM'] = 'Adresse bestatigen';
+  $sh_LANG['de']['_COM_SEF_LM_SUBSCRIBE'] = 'Eintragen';
+  $sh_LANG['de']['_COM_SEF_LM_UNSUBSCRIBE'] = 'Abmelden';
+  
+  // hungarian : translation by Jozsef Tamas Herczeg 2007-05-25
+  $sh_LANG['hu']['_COM_SEF_LETTERMAN'] = 'Hírlevél';
+  $sh_LANG['hu']['_COM_SEF_LM_CONFIRM'] = 'Cím megerõsítése';
+  $sh_LANG['hu']['_COM_SEF_LM_SUBSCRIBE'] = 'Megrendelés';
+  $sh_LANG['hu']['_COM_SEF_LM_UNSUBSCRIBE'] = 'Lemondás';
+
+  // italian
+  $sh_LANG['it']['_COM_SEF_LETTERMAN'] = 'Newsletter';
+  $sh_LANG['it']['_COM_SEF_LM_CONFIRM'] = 'Conferma indirizzo';
+  $sh_LANG['it']['_COM_SEF_LM_SUBSCRIBE'] = 'Iscriviti';
+  $sh_LANG['it']['_COM_SEF_LM_UNSUBSCRIBE'] = 'Cancella iscrizione';
+  
+  // dutch
+  $sh_LANG['nl']['_COM_SEF_LETTERMAN'] = 'Nieuwsbrief';
+  $sh_LANG['nl']['_COM_SEF_LM_CONFIRM'] = 'Keur adres goed';
+  $sh_LANG['nl']['_COM_SEF_LM_SUBSCRIBE'] = 'Inschrijven';
+  $sh_LANG['nl']['_COM_SEF_LM_UNSUBSCRIBE'] = 'Uitschrijven';
+  
+?>
