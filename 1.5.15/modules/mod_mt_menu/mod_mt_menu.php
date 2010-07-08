86804c9611851126ecdece647f9bb68ea14a5a66
ally <ally@478a4fa8-67ee-0310-8774-9eb661efe622>
2010-07-08 03:33:01 +0000

    modify user allow to add list and modify power

--- 1.5.15/modules/mod_mt_menu/mod_mt_menu.php
+++ 1.5.15/modules/mod_mt_menu/mod_mt_menu.php
@@ -13,21 +13,25 @@ defined('_JEXEC') or die('Restricted access');
 require_once (dirname(__FILE__).DS.'helper.php');
 include( JPATH_ROOT.DS.'components'.DS.'com_mtree'.DS.'init.php');
 
-$my			=& JFactory::getUser();
+$my		=& JFactory::getUser();
 $cat_id		= JRequest::getInt('cat_id', 0, 'get');
 $link_id	= JRequest::getInt('link_id', 0, 'get');
 $user_id	= JRequest::getInt('user_id', 0);
+//get user group id
+$user_gid = $my->get('gid');
 
 // Get params
-$class_sfx						= $params->get( 'class_sfx' );
-$moduleclass_sfx				= $params->get( 'moduleclass_sfx' );
-$limit_toplist 					= $params->get( 'limit_toplist', 0 );
+if ($user_gid >'19' && $user_gid < '28'){
+	
+$class_sfx				= $params->get( 'class_sfx' );
+$moduleclass_sfx			= $params->get( 'moduleclass_sfx' );
+$limit_toplist 				= $params->get( 'limit_toplist', 0 );
 $show['addlisting_force']		= $params->get( 'show_addlisting_force', 0 );
-$show['home'] 					= $params->get( 'show_home', 0 );
-$show['browse'] 				= $params->get( 'show_browse', 1 );
+$show['home'] 				= $params->get( 'show_home', 0 );
+$show['browse'] 			= $params->get( 'show_browse', 1 );
 $show['addlisting'] 			= $params->get( 'show_addlisting', 1 );
 $show['addcategory'] 			= $params->get( 'show_addcategory', 1 );
-$show['mypage'] 				= $params->get( 'show_mypage', 1 );
+$show['mypage'] 			= $params->get( 'show_mypage', 1 );
 $show['myfavourites']			= $params->get( 'show_myfavourites', 1 );
 $show['newlisting'] 			= $params->get( 'show_newlisting', 1 );
 $show['recentlyupdatedlisting']	= $params->get( 'show_recentlyupdatedlisting', 1 );
@@ -36,7 +40,12 @@ $show['featuredlisting']		= $params->get( 'show_featuredlisting', 1 );
 $show['popularlisting']			= $params->get( 'show_popularlisting', 1 );
 $show['mostratedlisting']		= $params->get( 'show_mostratedlisting', 1 );
 $show['topratedlisting'] 		= $params->get( 'show_topratedlisting', 1 );
-$show['mostreviewedlisting']	= $params->get( 'show_mostreviewedlisting', 1 );
+$show['mostreviewedlisting']		= $params->get( 'show_mostreviewedlisting', 1 );
+}
+if ( $user_gid = 0 or $user_gid =19 ){
+echo JText::_('SUGGEST_ITEMS');
+}
+
 
 // Get Link's category
 if ( $link_id > 0 && $cat_id == 0 ) {
