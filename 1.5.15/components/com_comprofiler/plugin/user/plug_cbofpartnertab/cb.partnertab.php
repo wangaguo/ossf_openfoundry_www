7dfffc563ecae74b77ae07c5dbf3fb81955b1251
kaworu <kaworu@478a4fa8-67ee-0310-8774-9eb661efe622>
2010-07-13 08:06:14 +0000

    fix ticket#37671 with river

--- 1.5.15/components/com_comprofiler/plugin/user/plug_cbofpartnertab/cb.partnertab.php
+++ 1.5.15/components/com_comprofiler/plugin/user/plug_cbofpartnertab/cb.partnertab.php
@@ -13,8 +13,10 @@ class getPartnerTab extends cbTabHandler {
 		global $_CB_framework, $_CB_database, $mainframe;
 		$username =str_replace ("!","",$user->username);
 		$live_site	=	$_CB_framework->getCfg( 'live_site' );
+		$lang = JFactory::getLanguage()->getTag();
+		if($lang == 'en-GB')$lang='en';
 	    	$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $live_site."/of/api/user?do=partners&name=$username");
+		curl_setopt($ch, CURLOPT_URL, $live_site."/of/api/user?do=partners&name=$username&lang=$lang");
         	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 ); 
 	    	$output = curl_exec($ch);
 		curl_close($ch); 
