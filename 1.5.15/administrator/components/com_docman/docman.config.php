82cc3e28d3c97414fd9b4ef78b8fb2edefa51e3a
ally <ally@478a4fa8-67ee-0310-8774-9eb661efe622>
2011-04-19 06:41:12 +0000

    docman config setting file

--- 1.5.15/administrator/components/com_docman/docman.config.php
+++ 1.5.15/administrator/components/com_docman/docman.config.php
@@ -6,8 +6,8 @@ define('_dmConfig',1);
 
 class dmConfig
 {
-// Last Edit: Wed, 2010-Jul-07 09:52
-// Edited by: OSSFHelper
+// Last Edit: Thu, 2011-Apr-14 12:16
+// Edited by: webmaster
 var $author_can = '2';
 var $days_for_new = '5';
 var $default_editor = '-6';
@@ -20,7 +20,7 @@ var $dmpath = '/usr/local/www/wsw/dmdocuments';
 var $docman_version = '1.4.0.stable';
 var $editor_assign = '2';
 var $emailgroups = '0';
-var $extensions = 'zip|rar|pdf|txt';
+var $extensions = 'zip|rar|pdf|txt|odp';
 var $fname_blank = '0';
 var $fname_lc = '0';
 var $fname_reject = 'index.htm|index.html|index.php';
@@ -32,14 +32,14 @@ var $individual_perm = '1';
 var $isDown = '0';
 var $log = '0';
 var $maintainer = '1';
-var $maxAllowed = '10485760';
+var $maxAllowed = '31457280';
 var $methods = array (
   0 => 'http',
   1 => 'link',
   2 => 'transfer',
 );
-var $overwrite = '0';
-var $perpage = '5';
+var $overwrite = '1';
+var $perpage = '30';
 var $process_bots = '0';
 var $reader_assign = '3';
 var $registered = '2';
