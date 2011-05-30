f4304fffdfc9b87aa1b27b0246c32ba8cd6828b5
ally <ally@478a4fa8-67ee-0310-8774-9eb661efe622>
2011-05-30 07:50:57 +0000

    custome page title

--- trunk/1.5.15/templates/rt_quantive_j15/index.php
+++ trunk/1.5.15/templates/rt_quantive_j15/index.php
@@ -21,6 +21,11 @@ $gantry->init();
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $gantry->language; ?>" lang="<?php echo $gantry->language;?>" >
 <head>
+<?php
+$app = JFactory::getApplication();
+$this->setTitle( $this->getTitle() . ' - ' . $app->getCfg( 'sitename' ) );
+?>
+
 <link rel="shortcut icon" href="/images/favicon.ico" />
 
 	<?php 
