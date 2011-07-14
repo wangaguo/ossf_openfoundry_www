b202b653fa5ec8150488cb78fa42c87681eb3e7b
ally <ally@478a4fa8-67ee-0310-8774-9eb661efe622>
2011-07-14 07:28:08 +0000

    change addthis module

--- 1.5.15/templates/rt_quantive_j15/html/com_content/article/default.php
+++ 1.5.15/templates/rt_quantive_j15/html/com_content/article/default.php
@@ -101,15 +101,15 @@ $canEdit	= ($this->user->authorize('com_content', 'edit', 'content', 'all') || $
 		<?php echo $this->article->text; ?>
 		<br>
 		<div class="add_this">	
+
 			<!-- AddThis Button BEGIN -->
-			<div class="addthis_toolbox addthis_default_style ">
-			<a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
-			<a class="addthis_button_tweet"></a>
-			<a class="addthis_counter addthis_pill_style"></a>
-			</div>
-			<script type="text/javascript">var addthis_config = {"data_track_clickback":true};</script>
-			<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=openfoundry"></script>
-			<!-- AddThis Button END -->
+			<div class="addthis_toolbox addthis_default_style addthis_32x32_style">
+			<a class="addthis_button_preferred_1"></a>
+			<a class="addthis_button_preferred_2"></a>
+			<a class="addthis_button_preferred_3"></a>
+			<a class="addthis_button_preferred_4"></a>
+			<a class="addthis_button_compact"></a>
+			<a class="addthis_counter addthis_bubble_style"></a>
 			</div>
 			<script type="text/javascript">var addthis_config = {"data_track_clickback":true};</script>
 			<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=openfoundry"></script>
