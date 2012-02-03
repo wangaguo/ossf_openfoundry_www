1690ec4401b6408a6764c4c3a647254aef429578
ally <ally@478a4fa8-67ee-0310-8774-9eb661efe622>
2012-02-03 09:29:26 +0000

    modify style and change searchphrase to new params: exact_meta

--- trunk/1.5.15/modules/mod_easytagcloud/tmpl/default.php
+++ trunk/1.5.15/modules/mod_easytagcloud/tmpl/default.php
@@ -20,7 +20,7 @@ div#easytagcloud<?php echo $moduleclass_sfx ?> a {
 
 -->
 </style>
-<div id="easytagcloud<?php echo $moduleclass_sfx ?>" style="text-align:justify;">
+<div id="easytagcloud<?php echo $moduleclass_sfx ?>" >
 <?php
  foreach($easytagcloud_params->tagsarray as $key=>$value) 
   {    
@@ -36,14 +36,14 @@ div#easytagcloud<?php echo $moduleclass_sfx ?> a {
      $sef_rewrite=$app->getCfg('sef_rewrite')==0?'index.php/':'';     
      if($app->getCfg('sef')==0)	
 	   {        
-	    $searchphrase="<a href='".JURI::base()."index.php?option=com_search&amp;Itemid=1&amp;searchword=".urlencode($key)."&amp;submit=Search&amp;searchphrase=exact&amp;ordering=newest'  style='font-size:".$easytagcloud_params->tagsstyle[$key]."em' title='".$value." $tip' target='_blank'>".$key."</a>";   
+	    $searchphrase="<a href='".JURI::base()."index.php?option=com_search&amp;Itemid=1&amp;searchword=".urlencode($key)."&amp;submit=Search&amp;searchphrase=exact_meta&amp;ordering=newest'  style='font-size:".$easytagcloud_params->tagsstyle[$key]."em' title='".$value." $tip' target='_blank'>".$key."</a>";   
 	    }
 	    else if($app->getCfg('sef')==1&&$app->getCfg('sef_suffix')==0)	
 		       {   
-			    $searchphrase="<a href='".JURI::base().$sef_rewrite."component/search/".urlencode($key)."?searchphrase=exact&amp;ordering=newest'  style='font-size:".$easytagcloud_params->tagsstyle[$key]."em' title='".$value." $tip' target='_blank'>".$key."</a>";    }	
+			    $searchphrase="<a href='".JURI::base().$sef_rewrite."component/search/".urlencode($key)."?searchphrase=exact_meta&amp;ordering=newest'  style='font-size:".$easytagcloud_params->tagsstyle[$key]."em' title='".$value." $tip' target='_blank'>".$key."</a>";    }	
 			    else if($app->getCfg('sef')==1&&$app->getCfg('sef_suffix')==1)	
 				       {  
-					    $searchphrase="<a href='".JURI::base().$sef_rewrite."component/search/".urlencode($key).".html?searchphrase=exact&amp;ordering=newest'  style='font-size:".$easytagcloud_params->tagsstyle[$key]."em' title='".$value." $tip' target='_blank'>".$key."</a>";   
+					    $searchphrase="<a href='".JURI::base().$sef_rewrite."component/search/".urlencode($key).".html?searchphrase=exact_meta&amp;ordering=newest'  style='font-size:".$easytagcloud_params->tagsstyle[$key]."em' title='".$value." $tip' target='_blank'>".$key."</a>";   
 					    }	
    echo $searchphrase;
    echo " "; 
