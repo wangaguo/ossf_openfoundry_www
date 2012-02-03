907c0c96b476230575b13933549ef3fcceb168a7
ally <ally@478a4fa8-67ee-0310-8774-9eb661efe622>
2012-02-03 07:40:50 +0000

    add new search case for tag cloud  : exact_meta

--- 1.5.15/plugins/search/content.php
+++ 1.5.15/plugins/search/content.php
@@ -86,7 +86,12 @@ function plgSearchContent( $text, $phrase='', $ordering='', $areas=null )
 			$wheres2[] 	= 'a.metadesc LIKE '.$text;
 			$where 		= '(' . implode( ') OR (', $wheres2 ) . ')';
 			break;
-
+		case 'exact_meta':
+			$text		= $db->Quote( '%'.$db->getEscaped( $text, true ).'%', false );
+			$wheres2 	= array();
+			$wheres2[] 	= 'a.metakey LIKE '.$text;
+			$where 		= '(' . implode( ') OR (', $wheres2 ) . ')';
+			break;
 		case 'all':
 		case 'any':
 		default:
