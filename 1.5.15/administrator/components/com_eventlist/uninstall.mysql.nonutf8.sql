687eeab52916ccb1ed7a9a8c1fd952b43a71e28e
ally <ally@478a4fa8-67ee-0310-8774-9eb661efe622>
2012-08-14 09:45:29 +0000

    delete old eventlist

--- 1.5.15/administrator/components/com_eventlist/uninstall.mysql.nonutf8.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS 
-`#__eventlist_events`,
-`#__eventlist_venues`,
-`#__eventlist_categories`,
-`#__eventlist_groups`,
-`#__eventlist_groupmembers`,
-`#__eventlist_register`,
-`#__eventlist_settings`;
