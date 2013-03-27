74ff113f996e4b3133b2517efcf70db48aa90d1e
chiuyun <chiuyun@478a4fa8-67ee-0310-8774-9eb661efe622>
2013-03-27 03:57:31 +0000

    add captcha

---
+++ 1.5.15/captcha/configuration.php
@@ -0,0 +1,73 @@
+<?
+
+// -------------------------------------------------------------------------------------
+// Captcha Genie PHP Configuration File.
+// Copyright (c) 2010 Alexandru Marias. All rights reserved
+// Web: http://www.captchagenie.com
+// Phone: +40722486348
+// ----------------------------------------------------------------
+
+// You can customize the look and feel of the Captcha Image by changing the settings below.
+// We recommend that you save a backup copy of the initial configuration file, because if you
+// make unwanted or inappropriate changes that result in the script not working, you will be
+// able to easily restore it back
+
+// The font used, can be 1 if random chosed from a fonts directory or 0 if specified
+
+
+$CSize = 0;
+
+$CSizeWidth = 120;
+
+$CSizeHeight = 40;
+
+$CSrc = 'abcdefghijkmnpqrstuvwxyz23456789';
+
+$CType = 0;
+
+$CMinSize = 4;
+
+$CMaxSize = 5;
+
+$CBackgroundType = 2;
+
+$CBackgroundColor = '#FFFFFF';
+
+$CBackgroundFile = 'backgrounds/bluecircles.gif';
+
+$CBackgroundFillType = 1;
+
+$CFontUsed = 0;
+
+$CFontURL = 'fonts/font2.ttf';
+
+$FontNo = 34;
+
+$fonts_dir = 'fonts';
+
+$CFontColorType = 2;
+
+$CFontColor = '#2E2E27';
+
+$CFontSizeMin = 25;
+
+$CFontSizeMax = 30;
+
+$CFontRotMin = -15;
+
+$CFontRotMax = 15;
+
+$CFontPadding = 0;
+
+$CNoise = 1;
+
+$CNoiseType = 4;
+
+$CNoiseColor = '#336699';
+
+$output_type = 'png';
+
+$captcha_type = 1;
+
+
+ ?>
